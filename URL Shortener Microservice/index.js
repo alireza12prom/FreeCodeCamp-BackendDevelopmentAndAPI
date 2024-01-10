1706cf72b1e753beb942cbbc0a3128602224dfc6
Alireza <alirezax69prom@gmail.com>
2024-01-10 21:16:52 +0330

    Third solution

---
+++ URL Shortener Microservice/index.js
@@ -0,0 +1,44 @@
+const table = new Map();
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/shorturl", function (req, res) {
+  let { url } = req.body;
+
+  if (!URL.canParse(url)) {
+    res.json({ error: "invalid url" });
+    return;
+  }
+
+  url = new URL(url).toString();
+
+  // make a hash of url
+  let hash = 0;
+  for (let i = 0; i < url.length; i++) {
+    hash += url.charCodeAt(i);
+  }
+
+  // add to table
+  table.set(`${hash}`, url);
+
+  res.json({ original_url: url, short_url: hash });
+});
+
+app.get("/api/shorturl/:short_url", (req, res, next) => {
+  let url = table.get(req.params.short_url);
+  res.redirect(301, url);
+});
+
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log(`Listening on port ${listener.address().port}`);
+});
