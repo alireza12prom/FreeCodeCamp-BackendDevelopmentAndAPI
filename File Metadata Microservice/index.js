c3cd64dd3a93183aca2fa8df3a057d8e60f1d7c2
Alireza <alirezax69prom@gmail.com>
2024-01-10 21:19:16 +0330

    Fifth solution

---
+++ File Metadata Microservice/index.js
@@ -0,0 +1,25 @@
+var cors = require("cors");
+var express = require("express");
+var fileupload = require("express-fileupload");
+var app = express();
+
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", fileupload({}), (req, res, next) => {
+  const uploadedFile = req.files["upfile"];
+
+  res.json({
+    name: uploadedFile.name,
+    type: uploadedFile.mimetype,
+    size: uploadedFile.size,
+  });
+});
+
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
